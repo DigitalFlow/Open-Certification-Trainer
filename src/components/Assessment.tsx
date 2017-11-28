aa9e342265239ae895d74ec3134d055a38df299e
Florian Krönert <Florian.Kroenert@silflo.de>
2017-11-28 21:49:04 +0100

    Showing list of correctly and incorrectly answered questions after assessment

--- src/components/Assessment.tsx
+++ src/components/Assessment.tsx
@@ -18,6 +18,8 @@ interface AssessmentState {
   questionState: QuestionState;
   checkedAnswers: IAssociativeArray<boolean>;
   session: AssessmentSession;
+  showCorrectQuestions: boolean;
+  showIncorrectQuestions: boolean;
 }
 
 enum QuestionState {
@@ -35,7 +37,9 @@ export default class Assessment extends React.Component<IBaseProps, AssessmentSt
       checkingAnswers: false,
       questionState: QuestionState.Open,
       checkedAnswers: {},
-      session: new AssessmentSession({ sessionId: uuid(), certification: null, answers: {}})
+      session: new AssessmentSession({ sessionId: uuid(), certification: null, answers: {}}),
+      showCorrectQuestions: false,
+      showIncorrectQuestions: false
     } as AssessmentState;
   }
 
@@ -52,6 +56,8 @@ export default class Assessment extends React.Component<IBaseProps, AssessmentSt
       this.answerChangedHandler = this.answerChangedHandler.bind(this);
       this.reset = this.reset.bind(this);
       this.resetSession = this.resetSession.bind(this);
+      this.toggleShowCorrectQuestions = this.toggleShowCorrectQuestions.bind(this);
+      this.toggleShowIncorrectQuestions = this.toggleShowIncorrectQuestions.bind(this);
   }
 
   answerChangedHandler(answer: Answer){
@@ -112,6 +118,14 @@ export default class Assessment extends React.Component<IBaseProps, AssessmentSt
     }
 
     if (this.state.session && nextState.session && this.state.session.sessionId !== nextState.session.sessionId){
+      return true;
+    }
+
+    if (this.state.showCorrectQuestions !== nextState.showCorrectQuestions) {
+      return true;
+    }
+
+    if (this.state.showIncorrectQuestions !== nextState.showIncorrectQuestions) {
       return true;
     }
 
@@ -274,6 +288,14 @@ export default class Assessment extends React.Component<IBaseProps, AssessmentSt
       });
   }
 
+  toggleShowCorrectQuestions() {
+    this.setState({showCorrectQuestions: !this.state.showCorrectQuestions});
+  }
+
+  toggleShowIncorrectQuestions() {
+    this.setState({showIncorrectQuestions: !this.state.showIncorrectQuestions});
+  }
+
   render(){
       let content = (<div>Please select a course from the sidenav</div>);
 
@@ -292,7 +314,7 @@ export default class Assessment extends React.Component<IBaseProps, AssessmentSt
               <div>
                 <h1>{this.state.certification.name}</h1>
                 <ProgressBar striped now={progress} />
-                <QuestionView checkedAnswers={this.state.checkedAnswers} onAnswerChange={this.answerChangedHandler} question={activeQuestion} key={activeQuestion.key} highlightCorrectAnswers={this.state.checkingAnswers} highlightIncorrectAnswers={this.state.checkingAnswers} answersDisabled={this.state.checkingAnswers} />
+                <QuestionView checkedAnswers={this.state.checkedAnswers} onAnswerChange={this.answerChangedHandler} question={activeQuestion} key={activeQuestion.id} highlightCorrectAnswers={this.state.checkingAnswers} highlightIncorrectAnswers={this.state.checkingAnswers} answersDisabled={this.state.checkingAnswers} />
                 {this.state.questionState === QuestionState.Open ? (<Button onClick={this.checkAnswer}>Check Answer</Button>) : <div/>}
                 {this.state.questionState === QuestionState.Correct ? <span style={{color:"green"}}>Correct Response</span> : <div/>}
                 {this.state.questionState === QuestionState.Incorrect ? <span style={{color:"red"}}>Incorrect Response</span> : <div/>}
@@ -305,27 +327,30 @@ export default class Assessment extends React.Component<IBaseProps, AssessmentSt
           {
             let questions = this.state.certification.questions;
             let questionCount = questions.length;
+            let correctAnswers = new Array<string>();
 
-            let correctAnswers = questions.reduce((acc: number, val: Question) => {
+            let correctAnswerCount = questions.reduce((acc: number, val: Question) => {
               let answeredCorrectly = this.checkIfAnsweredCorrectly(val.answers, this.state.session.answers[val.id].reduce((acc: IAssociativeArray<boolean>, val: string) => {
                 acc[val] = true;
                 return acc;
               }, { }));
 
               if (answeredCorrectly){
+                correctAnswers.push(val.id);
                 return ++acc;
               }
               return acc;
             }, 0);
 
-            let resultPercentage = (correctAnswers / questionCount) * 100;
+            let resultPercentage = (correctAnswerCount / questionCount) * 100;
+            let text = null;
 
             if (resultPercentage >= 70)
             {
-              content = (
+              text = (
                 <div>
                   <h2>Congratulations!</h2>
-                  <p>You passed the exam with {correctAnswers} correct answers out of {questionCount} questions.
+                  <p>You passed the exam with {correctAnswerCount} correct answers out of {questionCount} questions.
                   <br/>
                   In respect to the 70% correct answer passing ratio, you passed having {resultPercentage}% of the questions answered correctly.
                   </p>
@@ -334,10 +359,10 @@ export default class Assessment extends React.Component<IBaseProps, AssessmentSt
             }
             else
             {
-              content = (
+              text = (
                 <div>
                   <h2>Sorry!</h2>
-                  <p>You did not pass the exam with {correctAnswers} correct answers out of {questionCount} questions.
+                  <p>You did not pass the exam with {correctAnswerCount} correct answers out of {questionCount} questions.
                   <br/>
                   In respect to the 70% correct answer passing ratio, you failed having {resultPercentage}% of the questions answered correctly.
                   Try again and don't give up!
@@ -345,6 +370,33 @@ export default class Assessment extends React.Component<IBaseProps, AssessmentSt
                 </div>
               );
             }
+
+            content = (
+              <div>
+                {text}
+                <Button onClick={this.toggleShowCorrectQuestions}>
+                  Correctly answered questions
+                </Button>
+                <Panel collapsible expanded={this.state.showCorrectQuestions}>
+                  {
+                    this.state.certification.questions.filter(q => correctAnswers.indexOf(q.id) !== -1).map(activeQuestion => {
+                      return <QuestionView checkedAnswers={this.state.session.answers[activeQuestion.id].reduce((acc, val) => {acc[val] = true; return acc;}, {} as IAssociativeArray<boolean>)} question={activeQuestion} key={activeQuestion.id} highlightCorrectAnswers={true} highlightIncorrectAnswers={true} answersDisabled={true} />;
+                    })
+                  }
+                </Panel>
+
+                <Button onClick={this.toggleShowIncorrectQuestions}>
+                  Incorrectly answered questions
+                </Button>
+                <Panel collapsible expanded={this.state.showIncorrectQuestions}>
+                {
+                  this.state.certification.questions.filter(q => correctAnswers.indexOf(q.id) === -1).map(activeQuestion => {
+                    return <QuestionView checkedAnswers={this.state.session.answers[activeQuestion.id].reduce((acc, val) => {acc[val] = true; return acc;}, {} as IAssociativeArray<boolean>)} question={activeQuestion} key={activeQuestion.id} highlightCorrectAnswers={true} highlightIncorrectAnswers={true} answersDisabled={true} />;
+                  })
+                }
+                </Panel>
+              </div>
+            )
           }
         }
         else
